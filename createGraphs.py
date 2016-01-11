93b758d2400dfeb57dd4c154c630c44a44a1dfd5
hlhendy <hlhendy@gmail.com>
2016-01-11 15:08:17 -0500

    change to createGraphs.py

---
+++ createGraphs.py
@@ -0,0 +1,11 @@
+"""INPUT: binary contact map as adjacency matrix
+
+   PROCESSES:
+	GQ: create graph model for Q
+	VQ: GQ is embedded in into a vector V using the attributes
+   
+   OUTPUT: output vectors as .arff and .csv files for ML """
+
+
+
+ 
