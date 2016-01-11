2f7df5a4ad8f9a5ee67808829bd3fa66a375c49f
hlhendy <hlhendy@gmail.com>
2016-01-11 14:24:15 -0500

    Added atttribute code

--- GraphAttributes.py
+++ GraphAttributes.py
@@ -4,26 +4,39 @@ def eigenvalueAttributes(graph):
 	L = nx.normalized_laplacian_matrix(graph)
 	e = numpy.linalg.eigvals(L.A)
 	#energy: squared sum of eigenvalues
-	sum = 0
+	eig_sum = 0
+	largest = 0
+	second_largest = 0
 	for i in e:
-		sum += i
-	return_values.append(sum*sum)
-	#Second largest eigenvalue
-	
-	#Number distict eigenvalues
-	
+		eig_sum += i
+		if math.abs(i) > largest:
+			largest = i
+		elif math.abs(i) > second_largest:
+			second_largest = i
+	return_values.append(eig_sum*eig_sum)
+	#Second largest |eigenvalue| <------------CHECK WHETHER ABS VAL
+	return_values.append(second_largest)
+	#Number distict eigenvalues <-------------NEED TO MAKE DISTICT VALS
+	return_values.append(len(e))
 	#Spectral Radius: largest |eigenvalue|
-	return_values.append(max(e))
+	return_values.append(largest)
 	return return_values
 	
 	
 def labelAttributes(graph):
 	return_values = []
 	#Link Impurity: number of edges {u, v} where L(u) != L(v) / number of edges
+	total_edges = len(graph.edges())
+	noMatchEdges = 0
+	#node list
+	nodeList = graph.nodes(data=True)
+	for i,j in enumerate(nodeList[:-1]):
+		if j != nodeList[i+1]:
+			noMatchEdges++
+	return_values.append(noMatchEdges/total_edges)		
 	#Neighborhood Impurity: vertex degree for only those with different label
 	#Label Entropy: For a graph G with k labels, 
 	##E(G) = - Sum(prob of given label * log prob of given label)
-	
 	return return_values
 	
 def clusterAttributes(graph):
@@ -33,6 +46,6 @@ def clusterAttributes(graph):
 	#Average clustering coefficient
 	return_values.append(clustering(graph))
 	#Small-worldness criteria
-	##generate random graph and get closeness/clustering
-	##compare with actual graph
+	 ##generate random graph and get closeness/clustering
+	 ##compare with actual graph
 	return return_values
